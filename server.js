2dbb58bf6f7e5675fe59df639d4903f976ca85e6
QQstone <qqqstone@163.com>
2019-08-15 17:28:49 +0800

    Update server.js
    function interfaces

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
 
 var cors = require('cors');
+
+var bodyParser = require('body-parser');
+
+var dns = require('dns')
+
+const urlModule = require('url')
 
 var app = express();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
+
+var websiteSave = require('./app.js').websiteSave;
+var getWebsite = require('./app.js').getWebsite
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -31,6 +36,44 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/shorturl/new",function(req,res){
+  var website = req.body.url;
+  console.log("request url:",website)
+  // validicate the website
+  var host = urlModule.parse(website).host
+  dns.lookup(host, function(err,address,family){
+    if(err){
+      console.log("lookup error:",err);
+      res.json({"error":"invalid URL"})
+    }else{
+      console.log('address: %j family: IPv%s', address, family)
+      if(!address){
+        res.json({"error":"invalid URL"})
+      }else{
+        websiteSave(website, function(err,data){
+          if(err){
+            console.log(err);
+          }else{
+            res.json(data)
+          }
+        })
+      }
+    }
+  })
+})
+
+app.get("/api/shorturl/:short_url",function(req,res){
+  getWebsite(req.params.short_url,function(err,data){
+    if(err){
+      res.end("can not find this short url")
+    }else{
+      console.log("found this",data)
+      res.redirect(301,data.original_url)
+    }
+  })
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
